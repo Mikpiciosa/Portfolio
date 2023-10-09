b62f2e8dffeb962a1924aebc706eb387f66c5f9e
Mikpiciosa <micagustina.m@gmail.com>
2023-10-09 16:40:10 -0300

    cambios

--- src/components/static/About/About.jsx
+++ src/components/static/About/About.jsx
@@ -1,26 +1,121 @@
 import "./About.css";
 import img from "../../../assets/imagenes/linkedin.jpg";
+import git from "../../../../public/github.svg";
+import mail from "../../../../public/mail.svg";
+import linkedin from "../../../../public/linkedin.svg";
+import { useState } from "react";
 
 export const About = () => {
-  return (
-    <div className="about__background">
-      <section className="contenedor__about">
-        <div className="about__contenedor--img">
-          <img className="about__img" src={img} alt="fotomia" />
-        </div>
-        <div className="contenedor__about--info">
-          <h2 className="contenedor__about--text">Sobre Mi</h2>
-          <p className="contenedor__about--p">
-            Hola, mi nombre es Micaela, soy desarrolladora y diseñadora web, me
-            encanta crear sitios llamativos y funcionales para los clientes.
-            <br /> <br />
-            Desde chica, me llamo la atención el diseño y cuando descubrí la
-            manera de combinarlo con código, encontré realmente mi profesión.
-          </p>
+  const [desc, setDesc] = useState(false);
+  function handleToggle() {
+    setDesc(!desc);
+    console.log(desc);
+  }
 
-          <button className="contenedor__about--boton">Leer más</button>
+  return (
+    <section>
+      <article>
+        <img src={img} alt="fotomia" />
+        <h2>Contacto</h2>
+        <div>
+          <img src={git} alt="git" />
+          <img src={linkedin} alt="linkedin" />
+          <img src={mail} alt="mail" />
         </div>
-      </section>
-    </div>
+      </article>
+      <article>
+        <div>
+          <div>
+            <h2>Sobre Mi</h2>
+          </div>
+          <div>
+            <p>
+              Hola! mi nombre es Micaela, soy desarrolladora y diseñadora web,
+              me encanta crear sitios llamativos y funcionales para los
+              clientes. Desde chica, me llamo la atención el diseño y cuando
+              descubrí la manera de combinarlo con código, encontré realmente mi
+              profesión.
+            </p>
+            <button>Leer más</button>
+            {/* Este se activa en PC */}
+            <div>
+              <h3>Skills</h3>
+              <ul>
+                <li>Capacidad analitica</li>
+                <li>Carismatica</li>
+                <li>Observadora</li>
+                <li>Buen trabajo en equipo</li>
+                <li>Detallista</li>
+                <li>Resolutiva</li>
+                <li>Creativa</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Habilidades</h3>
+            <div>
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+            </div>
+          </div>
+          <div>
+            <h3>Educacion</h3>
+            <ul>
+              <li>Diseño UX/UI: CoderHouse</li>
+              <li>Desarrollo Web: CoderHouse</li>
+              <li>Desarrollo Web Full Stack: Udemy</li>
+              <li>Diplomatura Desarrollo Web Full Stack: UTN</li>
+              <li>Git/GitHub: Coursera</li>
+              <li>
+                Habilidades blandas y Competencias laborales: Fundación Empujar.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Experiencia</h3>
+            <ul>
+              <li>
+                Maqueta, diseño y mantenimiento de páginas web en Wordpress.
+              </li>
+              <li>Contacto directo con clientes en función de la necesidad.</li>
+              <li>Gestión de la identidad de marca.</li>
+              <li>Diseño UX/UI.</li>
+              <li>
+                Elaboración de documentos para la planificación e implementación
+                posterior de la web.
+              </li>
+              <li>Programación en HTML y CSS, código embebido.</li>
+              <li>Actualización de sitios web.</li>
+            </ul>
+          </div>
+          {/* ESTE SE ACTIVA PARA MOBILE - es el mismo que el skills */}
+          <div>
+            <h3>Experiencia</h3>
+            <ul>
+              <li>
+                Maqueta, diseño y mantenimiento de páginas web en Wordpress.
+              </li>
+              <li>Contacto directo con clientes en función de la necesidad.</li>
+              <li>Gestión de la identidad de marca.</li>
+              <li>Diseño UX/UI.</li>
+              <li>
+                Elaboración de documentos para la planificación e implementación
+                posterior de la web.
+              </li>
+              <li>Programación en HTML y CSS, código embebido.</li>
+              <li>Actualización de sitios web.</li>
+            </ul>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 };
--- src/components/static/Portada/Portada.jsx
+++ src/components/static/Portada/Portada.jsx
@@ -3,9 +3,11 @@ import "./Portada.css";
 export const Portada = () => {
   return (
     <section className="portada">
-      <h2 className="portada__portfolio">Portfolio</h2>
       <div className="portada__contenedor">
-        <h1 className="portada__name">Micaela Muñoz</h1>
+        <h2 className="portada__portfolio">Portfolio</h2>
+        <h1 className="portada__name">
+          Micaela Muño<span>z</span>
+        </h1>
         <p className="portada__p">Desarrolladora Web Full Stack</p>
         <button className="portada__boton">Descargar CV</button>
       </div>
