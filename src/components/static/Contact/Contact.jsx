f13e3b9d3f0492be6faffc4d53d7d7ca8de3d57b
Mikpiciosa <micagustina.m@gmail.com>
2023-10-24 00:19:20 -0300

    cambios

--- src/components/static/Contact/Contact.jsx
+++ src/components/static/Contact/Contact.jsx
@@ -1,13 +1,33 @@
 import "./Contact.css";
+import mail from "/mail.svg";
+import linkedin from "/linkedin.svg";
+import git from "/github.svg";
 
 export const Contact = () => {
   return (
     <section className="contact--container">
       <div className="linear--right"></div>
       <h2 className="contact--title">Contacto</h2>
-      <p>Si tenes algún proyecto en mente, estaré encantada de escucharte.</p>
+      <div>
+        <a href="mailto:micagustina.m@gmail.com">
+          <img className="contact--img" src={mail} alt="" />
+        </a>
+        <a href="https://www.linkedin.com/in/micaela-oz/">
+          <img className="contact--img" src={linkedin} alt="" />
+        </a>
+        <a href="https://github.com/Mikpiciosa">
+          <img className="contact--img" src={git} alt="" />
+        </a>
+      </div>
+      <p className="contact--text">
+        Si tenes algún proyecto en mente, estaré encantada de escucharte.
+      </p>
       <form className="contact--form" action="">
-        <label htmlFor="">Hola</label>
+        <label htmlFor="">Nombre</label>
+        <input type="text" />
+        <label htmlFor="">Correo</label>
+        <input type="text" />
+        <label htmlFor="">Mensaje</label>
         <input type="text" />
       </form>
     </section>
--- src/components/static/Header/Header.jsx
+++ src/components/static/Header/Header.jsx
@@ -1,8 +1,24 @@
+import "./Header.css";
+import { useState } from "react";
 import Logo from "../../../assets/logo/cat_logo.webp";
+/*import hamg from "/hamburguesa.svg";  */
 
 export const Header = () => {
+  const [menu, setMenu] = useState(true);
+  console.log(menu, setMenu);
   return (
     <header>
+      {menu && (
+        <div className="header__menu">
+          <nav>
+            <ul>
+              <li className="header__menu--item">asd</li>
+              <li>123</li>
+              <li>21312s</li>
+            </ul>
+          </nav>
+        </div>
+      )}
       <nav>
         <img src={Logo} alt="logo" />
         {/* <ul>
